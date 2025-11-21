1f40ac267b3c4c3849a5593223170816ef022fe3
Nicolas Gomes de Almeida <109389725+RebelAstronomer@users.noreply.github.com>
2025-11-21 18:57:57 -0300

    First Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,30 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
-});
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': '/src',
+        '@components': '/src/components',
+        '@config': '/src/config',
+        '@lib': '/src/lib',
+        '@api': '/src/api',
+        '@pages': '/src/pages',
+        '@types': '/src/types',
+        '@utils': '/src/utils',
+        '@layouts': '/src/layouts',
+        '@styles': '/src/styles',
+        '@content': '/src/content',
+      },
+    },
+  },
+
+  integrations: [mdx(), sitemap()],
+})
---
+++ src/consts.ts
@@ -0,0 +1,14 @@
+export const SITE_TITLE: string = 'RebelAstronomer'
+export const SITE_DESCRIPTION: string = 'Welcome to my website!'
+export const SITE_TAGS = [
+  'Game Dev',
+  'React',
+  'Typescript',
+  'Astro',
+  'Odin',
+  'Raylib',
+  'Godot',
+  'Fullstack',
+  'FrontEnd',
+  'BackEnd',
+] as const
